f5b9b3a71e5ad38dcdd0ebe25123cf84d6bfca2d
wissaas <58660830+wissaas@users.noreply.github.com>
2020-01-13 18:59:20 +0200

    Add files via upload

---
+++ HelpGame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryPicture
+{
+    public partial class HelpGame : Form
+    {
+     //   Image helpgame = Image.FromFile(@"..\..\pictures\\helpgame.png");
+
+        public HelpGame()
+        {
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+            //   pictureBox1.Image = helpgame;
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            lists ls = new lists();
+            ls.Show();
+        }
+    }
+}
