c5a22c9427fb8443d9a17142936f515070a0a118
sahar <radollora@gmail.com>
2020-01-18 22:33:17 +0200

    FinalProject

---
+++ UnitTestProjectMemoryPic/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectMemoryPic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectMemoryPic")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5da472c3-c9b2-444a-9921-61697076f213")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProjectMemoryPic/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MemoryPicture;
+namespace UnitTestProjectMemoryPic
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //1
+            rmot f1 = new rmot("user1", 1);
+            Assert.IsFalse(f1.userName() == true);
+
+        }
+
+
+        public void TestMethod2()
+        {
+            Profile f1 = new Profile();
+            Assert.IsTrue(f1.ShowpicGold(90) == false);
+
+        }
+
+
+
+        public void TestMethod3()
+        {
+            Profile f1 = new Profile("user1");
+            Assert.IsTrue(f1.IfUser("user1") == true);
+        }
+
+
+
+
+        public void TestMethod4()
+        {
+            EndGame f1 = new EndGame();
+            Assert.IsFalse(f1.IfUser("user1") == true);
+
+        }
+
+
+
+        public void TestMethod5()
+        {
+            EndGame f1 = new EndGame();
+            Assert.IsFalse(f1.IfUser("user1") == true);
+
+        }
+    }
+}
